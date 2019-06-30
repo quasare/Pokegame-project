acdac41884c6c519df53515346ee118a17998800
Thomas Adams <tqsare@gmail.com>
2019-06-30 09:52:47 -0700

    Updates

---
+++ precache-manifest.4fee74b9d31fe3d456be97fdcf8fc7b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b234cc98391ba8e96a4957b5b7e4423e",
+    "url": "/quasare/Pokegame-project.git/index.html"
+  },
+  {
+    "revision": "ace7f895891ce3227310",
+    "url": "/quasare/Pokegame-project.git/static/css/main.e3ccdd15.chunk.css"
+  },
+  {
+    "revision": "73031686d5a1e8f110ef",
+    "url": "/quasare/Pokegame-project.git/static/js/2.2036385c.chunk.js"
+  },
+  {
+    "revision": "ace7f895891ce3227310",
+    "url": "/quasare/Pokegame-project.git/static/js/main.dfb21ee9.chunk.js"
+  },
+  {
+    "revision": "cf258faeb6853ddf2111",
+    "url": "/quasare/Pokegame-project.git/static/js/runtime~main.b1e2fc1a.js"
+  }
+]);
